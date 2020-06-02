6075da8e5fc912095c981d972beee676e6a3e376
Rafael Klossner <rafaelklossner@gmx.ch>
2020-06-02 11:35:08 +0200

    parse header flag

--- Notetaker.py
+++ Notetaker.py
@@ -1,15 +1,24 @@
+import argparse
 import datetime
 import os
 
 print("---------- NoteTaker ----------")
+
+parser = argparse.ArgumentParser(description='Notetaker')
+parser.add_argument('--header', action='store_true')
+args = vars(parser.parse_args())
+print(args["header"])
 
 print("Enter file name: ")
 file_name = input()
 
 if not os.path.isfile(file_name + ".md"):
     file = open(file_name + ".md", "w")
-    file.write("# " + file_name + "\n" + "\n")
-    file.write("<sup> Created by Rafael Klossner on " + str(datetime.datetime.now()) + " </sup>" + "\n")
+
+    if args["header"]:
+        file.write("# " + file_name + "\n" + "\n")
+        file.write("<sup> Created by Rafael Klossner on " + str(datetime.datetime.now()) + " </sup>" + "\n")
+
     file.close()
 
 os.system("code " + file_name + ".md")
